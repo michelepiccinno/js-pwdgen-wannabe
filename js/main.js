7e152b9fe6dc6ab0ee44fa9c1bc5340ecbfb7c67
michelepiccinno <michele.piccinno@gmail.com>
2023-06-29 18:24:01 +0200

    add-bonus

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 const firstName = prompt("Inserisci nome");
 const lastName = prompt("Inserisci cognome");
+const eta = prompt("Inserisci età");
 const prefColor = prompt("Inserisci il tuo colore preferito");
-const password = firstName + lastName + prefColor + parseInt(21);
+const password = firstName + lastName + prefColor + (parseInt(eta) + 21);
 
 console.log(password);
 
 document.getElementById("my-name").innerHTML = firstName;
 document.getElementById("my-lastname").innerHTML = lastName;
+document.getElementById("my-age").innerHTML = eta;
 document.getElementById("my-password").innerHTML = password;
-document.getElementById("my-message").innerHTML = "prova;"
